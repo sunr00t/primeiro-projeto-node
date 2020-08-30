d363bd4d1fc0b4d3831b6a2bb9ab27448d64059d
Bruno Nascimento <sunr00t@gmail.com>
2020-08-30 19:35:57 -0300

    Migration:CreateUse, changes on CreateAppointments

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,15 +6,19 @@ import {
   UpdateDateColumn,
 } from "typeorm";
 
-@Entity("appointments")
-class Appointment {
+@Entity("users")
+class User {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column()
-  provider: string;
-  @Column("time with time zone")
-  date: Date;
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -23,4 +27,4 @@ class Appointment {
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
