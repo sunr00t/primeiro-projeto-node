9081be6bae9cf083196284ba9cfebc1c99f40159
Bruno Nascimento <sunr00t@gmail.com>
2020-08-31 22:34:27 -0300

    Relationship ManyToOne with Model Appointment/User

--- src/database/migrations/1598822930671-CreateUsers.ts
+++ src/database/migrations/1598822930671-CreateUsers.ts
@@ -8,7 +8,7 @@ export class CreateUsers1598822930671 implements MigrationInterface {
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
             generationStrategy: "uuid",
             default: "uuid_generate_v4()",
