0e0f8941321e16a1499c65bc8bdb6fe5184376c6
Bruno Nascimento <sunr00t@gmail.com>
2020-09-14 21:05:18 -0300

    Changes on Appointment Services

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import usersRouter from "./users.routes";
 
 const routes = Router();
 
-routes.use("/appointments", usersRouter);
+routes.use("/appointments", appointmentsRouter);
 routes.use("/users", usersRouter);
 
 export default routes;
